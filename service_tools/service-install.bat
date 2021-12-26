b3b88bc3346652c0bf9d72d3796d7b62a8c6cd55
Andrea Malloni <malloni.andrea02@gmail.com>
2021-12-26 09:31:23 +0100

    fixed service install script and added uninstall script

--- service_tools/service-install.bat
+++
@@ -1,5 +0,0 @@
-nssm install "PathFinder" "pathfinder.exe"
-nssm set "PathFinder" AppDirectory %CD%
-nssm set "PathFinder" AppStdout %CD%\stdout.log
-nssm set "PathFinder" AppStderr %CD%\stderr.log
-nssm start "PathFinder"
---
+++ tools/uninstall.bat
@@ -0,0 +1,4 @@
+@echo off
+
+.\nssm stop "PathFinder"
+.\nssm remove "PathFinder" confirm
