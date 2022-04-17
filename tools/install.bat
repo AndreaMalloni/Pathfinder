1129bb16b432cd40284f5f502cf98862dde7f3ee
Andrea Malloni <malloni.andrea02@gmail.com>
2022-04-17 16:44:44 +0200

    removed log setting in install script

--- tools/install.bat
+++ tools/install.bat
@@ -2,5 +2,3 @@
 for %%a in ("%CD%") do set "root_folder=%%~dpa"
 
 .\nssm install "PathFinder" "%root_folder%pathfinder.exe"
-.\nssm set "PathFinder" AppStdout "%root_folder%logs\stdout.log"
-.\nssm set "PathFinder" AppStderr "%root_folder%logs\stderr.log"
